77add6920368ae307dcf6b94458814a32893c0d5
Hitotsubashi <1220043823@qq.com>
2023-08-04 21:31:22 +0800

    解决冲突，移除@types/mocha

--- packages/vscode-ext/client/src/tests/suite/index.ts
+++ packages/vscode-ext/client/src/tests/suite/index.ts
@@ -1,30 +1,36 @@
 import * as path from 'path';
-import { run } from 'jest-cli';
+// @ts-ignore
+import Mocha from 'mocha';
 import glob from 'glob';
-// export function runWithJest() {
-//   run([path.join(__dirname, 'extension.test.ts')]).then((value) => {
-//     console.log(value);
-//   });
-// }
 
-runWithJest();
+declare type Mocha = any;
 
-export function runWithJest(): Promise<void> {
-  const testFiles: string[] = [];
+export function run() {
+  // Create the mocha test
+  const mocha = new Mocha({
+    ui: 'tdd',
+  });
+
   const testsRoot = path.resolve(__dirname, '..');
 
-  return new Promise((c, e) => {
+  return new Promise((c: any, e: any) => {
     glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
       if (err) {
         return e(err);
       }
 
       // Add files to the test suite
-      files.forEach((f) => testFiles.push(path.resolve(testsRoot, f)));
+      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
       try {
         // Run the mocha test
-        run(testFiles);
+        mocha.run((failures: any) => {
+          if (failures > 0) {
+            e(new Error(`${failures} tests failed.`));
+          } else {
+            c();
+          }
+        });
       } catch (err) {
         console.error(err);
         e(err);
