297a0fe5762e2952102f815946d5d3dcae0a507d
Hitotsubashi <1220043823@qq.com>
2023-08-18 15:58:20 +0800

    save

--- packages/vscode-ext/test-fixtures/boundary/sagaroute.config.js
+++ packages/vscode-ext/test-fixtures/boundary/sagaroute.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require("path")
 
 module.exports = {
     pathRewrite: {'^./': '@/'},
@@ -6,7 +6,7 @@ module.exports = {
         gather: {
             beforeEach(fpath) {
               // form文件夹下的文件不会被解析
-              if (fpath.split(path.sep).includes('form')) {
+              if (fpath.includes(`${path.sep}form${path.sep}`)) {
                 return null;
               }
             },
