b869436aab4e48459cb4645fca2e26351916b063
Hitotsubashi <1220043823@qq.com>
2023-08-24 23:46:21 +0800

    修复window系统下markdown不跳转的bug

--- packages/vscode-ext/src/SagaRoute/hooks/statistic-path.ts
+++ packages/vscode-ext/src/SagaRoute/hooks/statistic-path.ts
@@ -1,37 +1,8 @@
 import { RoutingOption } from '@sagaroute/react';
 import { RouteObject } from '@sagaroute/react/lib/weave';
-import getPathCompletionItemManager, {
-  PathCompletionItemManager,
-} from '../../PathCompletionItemManager';
-import path from 'path';
+import getPathCompletionItemManager from '../../PathCompletionItemManager';
 
-let paths: { route: string; fpath?: string }[] = [];
 const pathCompletionItemManager = getPathCompletionItemManager();
-const routeToFilePathMap = new Map<RouteObject, string>();
-
-function traverseRoute(route: RouteObject, parentPath = '') {
-  let routePath = route.path ? `${parentPath}/${route.path}` : parentPath;
-  if (!routePath.startsWith('/')) {
-    routePath = '/' + routePath;
-  }
-  routePath = routePath.replace(/^\/\//, '/');
-  const routeAvaliable =
-    route.path &&
-    (route.element ||
-      route.Component ||
-      route.lazy ||
-      route.children?.some(({ index }) => index === true));
-
-  if (routeAvaliable) {
-    paths.push({ route: routePath, fpath: routeToFilePathMap.get(route) });
-  }
-
-  if (route.children?.length) {
-    route.children.forEach((item) => {
-      traverseRoute(item, routePath);
-    });
-  }
-}
 
 const statisticPathHooks: RoutingOption['hooks'] = {
   weave: {
@@ -39,25 +10,7 @@ const statisticPathHooks: RoutingOption['hooks'] = {
       order: 105,
       handler(route, imports, fileNode) {
         process.nextTick(() => {
-          if (fileNode.type === 'dir') {
-            const layoutFileNode = fileNode.children?.find(
-              ({ name }) => path.parse(name).name === '_layout',
-            );
-            if (layoutFileNode) {
-              routeToFilePathMap.set(route, layoutFileNode.path);
-            } else {
-              const indexFileNode = fileNode.children?.find(
-                ({ props, name }) =>
-                  props?.routeProps?.index === true ||
-                  (path.parse(name).name === 'index' && props?.routeProps?.index !== false),
-              );
-              if (indexFileNode) {
-                routeToFilePathMap.set(route, indexFileNode.path);
-              }
-            }
-          } else {
-            routeToFilePathMap.set(route, fileNode.path);
-          }
+          pathCompletionItemManager.establishMapWithRouteAndFilePath(route, fileNode);
         });
       },
     },
@@ -66,16 +19,7 @@ const statisticPathHooks: RoutingOption['hooks'] = {
       handler(routes: RouteObject[]) {
         // 放到宏任务里执行，避免阻塞生成路由列表
         process.nextTick(() => {
-          paths = [];
-          routes.forEach((route) => {
-            traverseRoute(route);
-          });
-          pathCompletionItemManager.setCompletions(
-            paths.map((item) =>
-              PathCompletionItemManager.transformPathToCompletionItem(item.route, item.fpath),
-            ),
-          );
-          routeToFilePathMap.clear();
+          pathCompletionItemManager.generateCompletions(routes);
         });
       },
     },
